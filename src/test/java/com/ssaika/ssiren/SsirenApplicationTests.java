7d8d794aa6ad3a5a1cdeee21dbf0e39561513ccf
JeongHunPark <127088039+JeongHunsss@users.noreply.github.com>
2026-06-06 23:50:07 +0900

    chore: init project (#3)

---
+++ src/test/java/com/ssaika/ssiren/SsirenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssaika.ssiren;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsirenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
