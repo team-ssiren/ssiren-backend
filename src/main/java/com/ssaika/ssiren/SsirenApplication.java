156d8a50dcfb69310bef585ae19d2f228c671225
ParkJeongHun <mizz1030@naver.com>
2026-06-06 23:33:30 +0900

    chore: init project

---
+++ src/main/java/com/ssaika/ssiren/SsirenApplication.java
@@ -0,0 +1,13 @@
+package com.ssaika.ssiren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsirenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsirenApplication.class, args);
+	}
+
+}
