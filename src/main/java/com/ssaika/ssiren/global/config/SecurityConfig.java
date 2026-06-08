43ee0de2da0533291621c7cf16db60beadd39b0c
YEON <gomsi0724@naver.com>
2026-06-08 13:38:28 +0900

    feat: 로컬 웹 개발용 CORS 설정 추가

--- src/main/java/com/ssaika/ssiren/global/config/SecurityConfig.java
+++ src/main/java/com/ssaika/ssiren/global/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,6 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .cors(Customizer.withDefaults())
             .csrf(AbstractHttpConfigurer::disable)
             .formLogin(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable)
